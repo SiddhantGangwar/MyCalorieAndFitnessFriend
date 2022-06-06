e3f16dfba41a0f98fe205731c06fdc796a620950
SiddhantGangwar <engineersiddhantcse@gmail.com>
2022-06-07 00:08:33 +0530

    adding home page frontend

--- MyCalorieAndFitnessFriend/MyCalorieAndFitnessFriend/settings.py
+++ MyCalorieAndFitnessFriend/MyCalorieAndFitnessFriend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrapform',
+    # 'bootstrapform',
 ]
 
 MIDDLEWARE = [
@@ -134,7 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT =os.path.join(BASE_DIR,'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
